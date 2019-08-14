03385f5aa02269702315727c69f6a4ade6872994
maarkeez <d.marquez.delgado.91@gmail.com>
2019-08-14 23:27:18 +0200

    feat(001): Random video
    - Simplified serve video

--- src/main/java/com/maarkeez/vss/Application.java
+++ src/main/java/com/maarkeez/vss/Application.java
@@ -2,14 +2,13 @@ package com.maarkeez.vss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
+
 @SpringBootApplication
-public class VideoStreamingServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VideoStreamingServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/main/java/com/maarkeez/vss/VideoController.java
+++ src/main/java/com/maarkeez/vss/VideoController.java
@@ -1,9 +1,6 @@
 package com.maarkeez.vss;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,26 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 @Service
 @RestController
 @RequiredArgsConstructor
 public class VideoController {
 
-    @Value("classpath:videos/minions_bomberos.mp4")
-    private Resource videoFile;
-
     @ResponseBody
-    @GetMapping("/vss/api/video/random.mp4")
-    public ResponseEntity<InputStreamResource> randomVideo() throws IOException {
-
-        InputStream videoStream = videoFile.getInputStream();
+    @GetMapping("/vss/api/ping")
+    public ResponseEntity<String> randomVideo() {
 
         return ResponseEntity.ok()
-                .contentLength(videoStream.available())
-                .contentType(MediaType.valueOf("video/mp4"))
-                .body(new InputStreamResource(videoStream));
+                .contentType(MediaType.TEXT_PLAIN)
+                .body("Pong");
     }
 }
