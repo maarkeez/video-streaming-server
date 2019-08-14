a40f8f46a1ca7a79ad664cab79fa3bf8b699ffe1
maarkeez <d.marquez.delgado.91@gmail.com>
2019-08-14 21:26:53 +0200

    feat(001): Random video
    - Fixed running from jar

--- src/main/java/com/maarkeez/vss/VideoController.java
+++ src/main/java/com/maarkeez/vss/VideoController.java
@@ -1,9 +1,7 @@
 package com.maarkeez.vss;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -22,14 +21,13 @@ import static java.nio.file.Files.readAllBytes;
 @RequiredArgsConstructor
 public class VideoController {
 
-    @Value("classpath:videos/minions_bomberos.mp4")
-    private Resource videoFile;
-
     @ResponseBody
     @GetMapping("/vss/api/video/random")
     public ResponseEntity<InputStreamResource> randomVideo() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File videoFile = new File(classLoader.getResource("videos/minions_bomberos.mp4").getFile());
 
-        byte[] videoBytes = readAllBytes(videoFile.getFile().toPath());
+        byte[] videoBytes = readAllBytes(videoFile.toPath());
         InputStream videoStream = new ByteArrayInputStream(videoBytes);
 
         return ResponseEntity.ok()
